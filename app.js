d4e041e5961e453ab91309b381ce920fb2df19a0
P Λ И D Λ <54446422+xPandaDev@users.noreply.github.com>
2022-01-10 00:01:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -123,5 +123,9 @@ function getParameter( parameterName ) {
 function clearStorage() {
     localStorage.clear()
     localStorage.setItem('backgroundImage', 'https://images.unsplash.com/photo-1494253188410-ff0cdea5499e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')
+    
+    var url = new URL(window.location.href)       
+    window.history.pushState('time', 'Time', url.pathname + '?time=2022-02-27')
+
     window.location.reload()
 }
